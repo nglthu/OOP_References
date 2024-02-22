338421bfb4e67398c6bbc70b4c8607d7a9977189
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-02-22 09:23:41 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int SiSoLop;
+        SiSoLop = 40;
+        Date d = new Date();
+        System.out.println(SiSoLop);
+        System.out.println(d);
+    }
+}
